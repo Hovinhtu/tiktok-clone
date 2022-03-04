d689e101c9882305b839b5881373593f6f533a6d
TuCalisthenic <hovinhtusqtt@gmail.com>
2022-03-04 19:19:48 +0700

    Nav-bar UI done without responsive

--- src/Header/js/IconHeader.js
+++ src/Header/js/IconHeader.js
@@ -2,12 +2,10 @@ import React from "react";
 import IconComponent from "./IconComponent";
 
 export default function IconHeader() {
-  return (
-    <div>
+  return ( 
       <a style={{ width: "118px", height: "42px",fontSize:"0px" }} href="#">
         <IconComponent />
         <strong>Tiktok</strong>
       </a>
-    </div>
   );
 }
